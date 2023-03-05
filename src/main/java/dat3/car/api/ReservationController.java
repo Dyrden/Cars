53a5a4f7f2106ea4f979ab9e32c40ca48cdc6169
Mark <mark771e@stud.kea.dk>
2023-03-05 03:07:37 +0100

    fixes

--- src/main/java/dat3/car/api/ReservationController.java
+++ src/main/java/dat3/car/api/ReservationController.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/reservation")
 public class ReservationController {
 

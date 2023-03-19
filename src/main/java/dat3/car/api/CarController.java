1d31b20b90ea0f0d40bf2a38c1d71999fe0c18a1
Mark <mark771e@stud.kea.dk>
2023-03-19 02:15:43 +0100

    authority

--- src/main/java/dat3/car/api/CarController.java
+++ src/main/java/dat3/car/api/CarController.java
@@ -6,6 +6,7 @@ import dat3.car.service.CarService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,24 +23,28 @@ public class CarController {
     }
 
     // USER
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
     @GetMapping
     List<CarResponse> getCars() {
         return carService.getCars();
     }
 
     //USER
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
     @GetMapping(path = "/{id}")
     CarResponse getCarById(@PathVariable int id) {
         return carService.getCarById(id);
     }
 
     //ADMIN
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     CarResponse addCar(@RequestBody CarRequest body) {
         return carService.addCar(body);
     }
 
     //ADMIN
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PutMapping("/{id}")
     ResponseEntity<Boolean> editCar(@RequestBody CarRequest body, @PathVariable int id) {
         carService.updateCar(body, id);
@@ -47,12 +52,14 @@ public class CarController {
     }
 
     //ADMIN
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PatchMapping("/bestdiscount/{id}/{value}")
     CarResponse setBestDiscountForCar(@PathVariable int id, @PathVariable int value) {
         return carService.updateCarDiscount(id, value);
     }
 
     //ADMIN
+    @PreAuthorize("hasAuthority('ADMIN')")
     @DeleteMapping("/{id}")
     ResponseEntity<Boolean> deleteCarById(@PathVariable int id) {
         carService.deleteCar(id);
